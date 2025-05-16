21e189168ee57074559e6346d8f449d144d3774a
Lucas LucidVR <35583218+lucas-vrtech@users.noreply.github.com>
2025-05-16 18:12:10 -0400

    Put profile methods back in (oops)

--- index.js
+++ index.js
@@ -29,7 +29,110 @@ createApp({
     removeURLFromUsername(username) {
       username = username.replace(/https?:\/\/[^\/]+\//, '');
       return username;
-    }
+    },
+
+    async fetchProfile(profile){
+      this.profileReady = false;
+      this.latestProfile = null;
+      this.editingProfile = false;
+      
+      this.profile = profile;
+      console.log("Fetching profile:", profile);
+      await this.getOrCreateProfile(this.$graffitiSession.value);
+    },
+
+    async createProfile(session){
+      console.log("Creating profile!");
+      this.thisProfile = {
+        value: { 
+          activity: 'Create',
+          object: {
+            type: 'Profile',
+            name: '',
+            email: '',
+            username: this.profile,
+            picURL: '',
+          }
+         },
+         channels: [this.profile],
+      }
+      await this.$graffiti.put(
+        this.thisProfile,
+        session,
+      );
+      this.latestProfile = this.thisProfile;
+      console.log("Created profile!");
+    },
+
+    async getOrCreateProfile(session) {
+      this.isLoading = true;
+      // First try to get the profile
+      const profileIterator = await this.$graffiti.discover([this.profile], {
+        value: {
+          properties: {
+            activity: { type: "string", const: "Create" },
+            object: {
+              type: { type: "string", const: "Profile" },
+              username: { type: "string", const: this.profile }
+            }
+          }
+        }
+      });
+
+      // Check if we found a profile
+      for await (const { object } of profileIterator) {
+        if (object.value.object.username === this.profile) {
+          this.profileReady = true;
+          console.log('Profile found:', object);
+          this.latestProfile = object;
+          this.isLoading = false;
+          return object;
+        }
+      }
+
+      // If no profile found, create a new one
+      await this.createProfile(session);
+      this.isLoading = false;
+    },
+
+    async updateProfile(profileData, session) {
+      this.isLoading = true;
+      
+      const profileToUpdate = profileData || this.latestProfile;
+      
+      console.log("Updating profile:", profileToUpdate);
+      
+      const updatedProfile = await this.$graffiti.patch(
+        {
+          value: [
+            {
+              op: "replace",
+              path: "/object/name",
+              value: profileToUpdate.value.object.name
+            },
+            {
+              op: "replace",
+              path: "/object/email",
+              value: profileToUpdate.value.object.email
+            },
+            {
+              op: "replace",
+              path: "/object/picURL",
+              value: profileToUpdate.value.object.picURL
+            }
+          ]
+        },
+        profileToUpdate,
+        session
+      );
+      
+      this.latestProfile = profileToUpdate;
+      this.editingProfile = false;
+      this.isLoading = false;
+      
+      return updatedProfile;
+    },
+
   }
 })
   .use(router)
